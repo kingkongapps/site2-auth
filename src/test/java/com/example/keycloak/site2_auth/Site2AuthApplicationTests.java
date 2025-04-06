dbfb1b61e96b1c5d92617bf4d2b7f6167908bf37
inmokang <inmo.kang@gmail.com>
2025-04-07 05:27:28 +0900

    first commit

---
+++ src/test/java/com/example/keycloak/site2_auth/Site2AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.site2_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Site2AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
